aa2aad17b36199c0c5d54d836acb760db5884cc1
Che <bche.21@ichat.sp.edu.sg>
2022-12-01 12:23:09 +0800

    Initial version to display text message

---
+++ bmi.py
@@ -0,0 +1,10 @@
+def calculate_bmi(height, weight):
+ print("Height = " + str(height))
+ print("Weight = " + str(weight))
+
+ #Add code here to calculate BMI
+ bmi = weight /( height * height)
+
+ #Add code here to display calculate BMI
+ print( bmi)
+calculate_bmi(weight=57, height=1.73)
